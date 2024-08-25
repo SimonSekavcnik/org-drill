3acd3343d5e3d671d47a0abd775fc3dbfed94f18
Simon Sekavcnik <simon.sekavcnik@gmail.com>
2024-08-25 14:32:18 +0200

    Fixed the bug

--- org-drill.el
+++ org-drill.el
@@ -719,8 +719,9 @@ CMD is bound, or nil if it is not bound to a key."
 (defun org-drill-time-to-inactive-org-timestamp (time)
   "Convert TIME into org-mode timestamp."
   (format-time-string
-   (concat "[" (cdr substring org-time-stamp-formats) "]")
+   (concat "[" (cdr org-time-stamp-formats) "]")
    time))
+
 
 (defun org-drill-map-entries (func &optional scope drill-match &rest skip)
   "Like `org-map-entries', but only drill entries are processed."
